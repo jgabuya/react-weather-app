944958944295066911ff7f71440a47da9238ef47
Jirffey Gabuya <boyhimas@gmail.com>
2017-12-18 03:15:08 +0800

    Add formatting, display more data

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Alert, InputGroup, InputGroupButton, Input, Button } from 'reactstrap';
+import isEmpty from 'lodash/isEmpty';
 
 class Main extends React.Component {
     constructor() {
@@ -50,6 +51,7 @@ class Main extends React.Component {
         return axios.get(instance.props.apiUrl, {
             params: {
                 q: location,
+                units: 'metric',
                 appid: instance.props.apiKey
             }
         })
@@ -58,7 +60,7 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                <div className={(( this.state.errorMessage.length == 0)? 'd-none ' : '') + 'row mb-3'}>
+                <div className={((this.state.errorMessage.length === 0)? 'd-none ' : '') + 'row mb-3'}>
                     <div className="col">
                         <Alert color="warning">
                             {this.state.errorMessage}
@@ -66,20 +68,33 @@ class Main extends React.Component {
                     </div>
                 </div>
 
-                <div className="row">
+                <div className="row mb-4">
                     <div className="col">
                         <form>
                             <InputGroup>
                                 <Input onChange={this.onLocationChange} placeholder="Enter your location" />
                                 <InputGroupButton><Button type="submit" onClick={this.onLocationSubmit}>Go!</Button></InputGroupButton>
                             </InputGroup>
-
-                            <div>
-                                {JSON.stringify(this.state.weatherData)}
-                            </div>
                         </form>
                     </div>
                 </div>
+
+                {! isEmpty(this.state.weatherData) &&
+                    <div className={((isEmpty(this.state.weatherData))? 'd-none ' : '') + 'row'}>
+                        <div className="col">
+                            <div className="jumbotron">
+                                <img src={'http://openweathermap.org/img/w/' + this.state.weatherData.weather[0].icon + '.png'} alt={this.state.weatherData.weather[0].icon} />
+                                
+                                <h1 className="display-4">{this.state.weatherData.name}</h1>
+
+                                <p className="lead">
+                                    {this.state.weatherData.main.temp} &deg;C, {this.state.weatherData.weather[0].main}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                }
+
             </div>
         )
     }
